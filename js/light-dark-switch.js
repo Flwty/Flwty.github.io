0972182cc97eeaebd510dbd00dddcc73c50b1389
Flwty <2283156488@qq.com>
2024-05-06 16:38:07 +0800

    Site updated: 2024-05-06 16:38:05

--- js/light-dark-switch.js
+++ js/light-dark-switch.js
@@ -2,12 +2,12 @@ let themeFunc = async function() {
     let btn = document.getElementById("theme-btn");
     let root = document.documentElement;
     btn.addEventListener('click', e => {
-        if (root.getAttribute('theme') == 'dark') {
-            root.setAttribute('theme', 'light');
-            localStorage.setItem('theme', 'light');
-        } else {
+        if (root.getAttribute('theme') == 'light') {
             root.setAttribute('theme', 'dark');
             localStorage.setItem('theme', 'dark');
+        } else {
+            root.setAttribute('theme', 'light');
+            localStorage.setItem('theme', 'light');
         }
     });
 };
